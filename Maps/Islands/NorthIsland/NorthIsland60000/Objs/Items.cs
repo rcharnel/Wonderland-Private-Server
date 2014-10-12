c86b323269fa7f7f5edd669e51dd1bc7f0adad9a
petroni oliver roche <petronaooliver@gmail.com>
2014-10-12 19:17:36 -0300

    test

---
+++ Maps/Islands/NorthIsland/NorthIsland60000/Objs/Items.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wonderland_Private_Server.Maps.Islands.NorthIsland.NorthIsland60000.Objs
+{
+    class Items
+    {
+    }
+}
